4b4edeee7cf14cb50cb4da130664fd3b7b1ace95
samirfoul <foul_s@hotmail.fr>
2022-11-26 12:14:47 +0100

    Projet-jQuery

---
+++ scriptProjet.js
@@ -0,0 +1,55 @@
+$(function(){
+  
+    var $mainMenuItems = $("#main-menu ul").children("li"),
+    totalMainMenuItems = $mainMenuItems.length,
+    openedIndex = -1,
+    
+    init = function(){
+
+
+        $mainMenuItems.children(".images").click(function(){
+           
+            var newIndex = $(this).parent().index(),
+            $item = $mainMenuItems.eq(newIndex);
+
+            if(openedIndex === newIndex)
+            {
+                animateItem($item, false, 250);
+                openedIndex = -1;
+            }
+            else
+            {
+                if(validIndex(newIndex))
+                {
+                    animateItem($mainMenuItems.eq(newIndex), false, 250); openedIndex = newIndex;
+            
+
+                    animateItem($item, true, 250);
+                }
+            }
+            
+
+        });
+    },    
+
+    validIndex = function(indexToCheck){
+        
+        return (indexToCheck >= 0) && (indexToCheck <  totalMainMenuItems) ;
+    },
+
+        animateItem = function($item, toOpen, speed){
+
+            var $colorImage = $item.find(".color"),
+            itemParam = toOpen ? {width:"420px"} : {width: "140px"} ,
+            colorImageParam = toOpen ? {left:"0px"}: {left:"140px"};
+            $colorImage.animate(colorImageParam,speed);
+            $item.animate(itemParam,speed);
+            
+
+        };
+    init();
+
+
+   
+
+});
